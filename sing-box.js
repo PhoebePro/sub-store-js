a0163e43aa4e237c822aa47d9acd7d0407fa255e
Pearce <44561721+PhoebePro@users.noreply.github.com>
2025-01-06 20:38:18 +0800

    Update sing-box.js

--- sing-box.js
+++ sing-box.js
@@ -32,7 +32,7 @@ config.outbounds.map(i => {
     i.outbounds.push(...getTags(proxies, /^(?=.*(新|sg|singapore|Singapore))(?!.*ACA).*$/i))
   }
   if (['🇺🇸 US'].includes(i.tag)) { 
-    i.outbounds.push(...getTags(proxies, /^(?!.*(Austria|Russia|[ACA])).*(美国|US|United\s*States).*/i))
+    i.outbounds.push(...getTags(proxies, /^(?!.*(Austria|Russia|[ACA])).*(美国|US|United States).*/i))
   }
   if (['Emby'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /ACA/))
